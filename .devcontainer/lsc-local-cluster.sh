c22a2853b081cf688ce515149b9b610bc13968b1
Michael Harding <m.harding@lancaster.ac.uk>
2024-04-25 16:09:33 +0000

    lsc-sde/lsc-sde#60 update to automate flux source and cluster config creation

--- .devcontainer/lsc-local-cluster.sh
+++ .devcontainer/lsc-local-cluster.sh
@@ -1,3 +1,7 @@
+LSCSDE_FLUX_REPO_URL="https://github.com/lsc-sde/iac-flux-lscsde"
+LSCSDE_FLUX_REPO_BRANCH="release/0.1.207"
+LSCSDE_FLUX_REPO_CLUSTER_PATH="./clusters/local"
+
 set -e
 
 echo "Delete lscsde local cluster if exists..."
@@ -7,5 +11,15 @@ echo "Create new lscsde local cluster..."
 k3d cluster create lscsde #> lscsde-k3d.log 2>&1
 kubectl cluster-info
 
-echo "Add flux operators to cluster..."
+echo "Add flux controllers to cluster..."
 flux install
+
+echo "Create lscsde and lscsde cluster namespaces..."
+kubectl create namespace lscsde
+kubectl create namespace lscsde-config
+
+echo "Create flux git source for lscsde..."
+flux create source git lscsde --url="${LSCSDE_FLUX_REPO_URL}" --branch="${LSCSDE_FLUX_REPO_BRANCH}" --namespace=lscsde
+
+echo "Create lscsde-cluster-config kustomization (may take a few seconds)..."
+flux create kustomization lscsde-cluster-config --source="GitRepository/lscsde" --namespace=lscsde --path="${LSCSDE_FLUX_REPO_CLUSTER_PATH}" --interval=1m --prune=true --health-check-timeout=2m --wait=false
